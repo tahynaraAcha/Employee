45100956f9bae410e73146cc07a26f8f9adf7b79
samuel.cabrera.1999@gmail.com <samuel.cabrera.1999@gmail.com>
2019-09-04 19:46:09 -0400

    clase creada

---
+++ Employee/Employee/Models/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Employee.Models
+{
+    enum typeEmployee
+    {
+
+
+    }
+
+
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+
+        public string Name { get; set; }
+
+        public int Salary { get; set; }
+
+        public string Birthday { get; set; }
+
+        public string Recomendation { get; set; }
+
+    }
+}
